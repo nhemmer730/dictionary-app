20c1edaca1d8061c429644bd19bba88d15da3b4a
nhemmer730 <nhemmer730@gmail.com>
2024-03-31 16:22:58 -0400

    Add search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">
+          <main>
+            <Dictionary />
+          </main>
+        </header>
+      </div>
     </div>
   );
 }
